b0deb638021a48fbece0e1b728ddde76f1c59e71
Eugenia Zelenka <eugeniazelenka@gmail.com>
2017-04-05 20:59:54 -0300

    arreglo de quicksort

--- src/quickSort/QuickSort.java
+++ src/quickSort/QuickSort.java
@@ -1,5 +1,7 @@
 package quickSort;
 
+import java.util.Stack;
+
 public class QuickSort {
 	Integer[] array;
 	Integer size;
@@ -37,31 +39,49 @@ public class QuickSort {
 		 
 	}
 	
-	//COMPLEJIDAD  EL PEOR CASO:O(n^2)  EL MEJOR CASO:O(nLOGn)
-	/*  LA COMPLEJIDAD VARÍA DEPENDIENDO DEL PIVOTE QUE SE ELIJA  */
-	
-	public void quickSort(int inicio, int fin) {
-		int i = inicio;
-		int j = fin;
-		int posPivote = calculaposPivote(inicio,fin);
+	public int partition(int inicio, int fin) {
+		int pivote = array[fin];
+		int i = inicio-1;
+		for(int j=inicio; j<fin; j++) {
+			if(array[j] <= pivote) {
+				i = i+1;
+				Integer aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+			}		
+		}
 		
-		while(array[i] < array[posPivote])
-			i++;
-		while(array[j] > array[posPivote])
-			j--;
-		
-		if(i <= j) {
-			Integer aux = array[i];
-			array[i] = array[j];
-			array[j] = aux;
-			i++;
-			j--;
- 		}
-		if(inicio < j)
-			quickSort(inicio, j);
-		if(i < fin)
-			quickSort(i,fin);
+		Integer aux = array[i+1];
+		array[i+1] = array[fin];
+		array[fin] = aux;	
+		return i+1;
 	}
 	
+	public void quickSortIterative() {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(0);
+		stack.push(array.length);
+		while(!stack.isEmpty()) {
+			int end = stack.pop();
+			int start = stack.pop();
+			if((end > start) ) {	
+				int p = partition(start, end);
+				stack.push(p+1);
+				stack.push(end);
+			
+				stack.push(start);
+				stack.push(p);
+			}	
+		}
+	}
+	
+	//COMPLEJIDAD  EL PEOR CASO:O(n^2)  EL MEJOR CASO:O(nLOGn)
+	/*  LA COMPLEJIDAD VARÍA DEPENDIENDO DEL PIVOTE QUE SE ELIJA  */
+	public void quickSort(int inicio, int fin) {
+		if(inicio < fin) {
+			int p = partition(inicio,fin);
+			quickSort(inicio, p-1);
+			quickSort(p+1, fin);
+		}
+	}
 }
-
